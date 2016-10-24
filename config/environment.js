8f047b3232ee8068557f462c77158c1f125a1090
Marta Sánchez and Zachary Richards <marta@>
2016-10-24 15:17:17 -0700

    finish working through base application instructions for homework

--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,13 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyCK6M295qFS836I673dtrT3oAVZXaHoKI8",
+      authDomain: "super-rentals-537fa.firebaseapp.com",
+      databaseURL: "https://super-rentals-537fa.firebaseio.com",
+      storageBucket: "super-rentals-537fa.appspot.com",
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
