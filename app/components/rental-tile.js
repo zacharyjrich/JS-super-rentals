d05e141745278690f519e04284d66369dceab17a
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-25 22:30:39 -0700

    Add application template, rental route for dynamic routing and review model

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -7,15 +7,7 @@ export default Ember.Component.extend({
       this.set('isImageShowing', true);
     },
     imageHide: function() {
-      this.set('isImageShowing', false);
-    },
-    update(rental, params) {
-      this.sendAction('update', rental, params);
-    },
-    delete(rental) {
-      if (confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
-    }
+     this.set('isImageShowing', false);
+   }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,28 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
-
   actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
     },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-
-    destroyRental(rental) {
-      rental.destroyRecord();
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      newReview.save();
       this.transitionTo('index');
     }
   }
